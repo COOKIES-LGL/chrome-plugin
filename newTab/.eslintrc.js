ed2922cfb358f3c60124545bc1ef88613a58d0f0
Guilin Li <guilin.li@shopee.com>
2021-08-21 21:57:11 +0800

    add eslint

---
+++ newTab/.eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  'extends': [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/typescript/recommended'
+  ],
+  parserOptions: {
+    ecmaVersion: 2020
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    '@typescript-eslint/no-explicit-any': ['off'], // 关闭后可以使用any
+    '@typescript-eslint/no-var-requires': 0, // 配置后可以使用var、requires
+  }
+}
